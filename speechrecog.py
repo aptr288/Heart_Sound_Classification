22f1f5be35178f1bb739a91b11692967bb40b22a
aptr288 <aptr288@gmail.com>
2018-06-10 23:00:28 +0530

    speech recognization python file

---
+++ speechrecog.py
@@ -0,0 +1,157 @@
+import librosa
+import os
+from sklearn.model_selection import train_test_split
+import numpy as np
+from tqdm import tqdm
+#from preprocess import *
+import keras
+from keras.models import Sequential
+from keras.layers import Dense, Dropout, Flatten, Conv2D, MaxPooling2D
+from keras.utils import to_categorical
+
+DATA_PATH = "C:/Users/apt/Desktop/Data/TF/"
+
+
+
+# Input: Folder Path
+# Output: Tuple (Label, Indices of the labels, one-hot encoded labels)
+def get_labels(path=DATA_PATH):
+    labels = os.listdir(path)
+    label_indices = np.arange(0, len(labels))
+    return labels, label_indices, to_categorical(label_indices)
+
+def prepare_dataset(path=DATA_PATH):
+    labels = os.listdir(path)
+    data = {}
+    for label in labels:
+        data[label] = {}
+        data[label]['path'] = [path  + label + '/' + wavfile for wavfile in os.listdir(path + '/' + label)]
+        
+        vectors = []
+
+        for wavfile in data[label]['path']:
+            wave, sr = librosa.load(wavfile, mono=True, sr=None)
+			# Downsampling
+            wave = wave[::3]
+            mfcc = librosa.feature.mfcc(wave, sr=16000)
+            vectors.append(mfcc)
+
+        data[label]['mfcc'] = vectors
+
+    return data
+
+# Handy function to convert wav2mfcc
+def wav2mfcc(file_path, max_len=11):
+    wave, sr = librosa.load(file_path, mono=True, sr=None)
+    wave = wave[::3]
+    mfcc = librosa.feature.mfcc(wave, sr=16000)
+
+    # If maximum length exceeds mfcc lengths then pad the remaining ones
+    if (max_len > mfcc.shape[1]):
+        pad_width = max_len - mfcc.shape[1]
+        mfcc = np.pad(mfcc, pad_width=((0, 0), (0, pad_width)), mode='constant')
+
+    # Else cutoff the remaining parts
+    else:
+        mfcc = mfcc[:, :max_len]
+    
+    return mfcc
+
+def save_data_to_array(path=DATA_PATH, max_len=11):
+    labels = os.listdir(path)
+
+    for label in labels:
+        # Init mfcc vectors
+        mfcc_vectors = []
+
+        wavfiles = [path + label + '/' + wavfile for wavfile in os.listdir(path + '/' + label)]
+        for wavfile in tqdm(wavfiles, "Saving vectors of label - '{}'".format(label)):
+            mfcc = wav2mfcc(wavfile, max_len=max_len)
+            mfcc_vectors.append(mfcc)
+        np.save(label + '.npy', mfcc_vectors)
+
+
+def get_train_test(split_ratio=0.6, random_state=42):
+    # Get available labels
+    labels, indices, _ = get_labels(DATA_PATH)
+
+    # Getting first arrays
+    X = np.load(labels[0] + '.npy')
+    y = np.zeros(X.shape[0])
+
+    # Append all of the dataset into one single array, same goes for y
+    for i, label in enumerate(labels[1:]):
+        x = np.load(label + '.npy')
+        X = np.vstack((X, x))
+        y = np.append(y, np.full(x.shape[0], fill_value= (i + 1)))
+
+    assert X.shape[0] == len(y)
+
+    return train_test_split(X, y, test_size= (1 - split_ratio), random_state=random_state, shuffle=True)
+
+
+def load_dataset(path=DATA_PATH):
+    data = prepare_dataset(path)
+
+    dataset = []
+
+    for key in data:
+        for mfcc in data[key]['mfcc']:
+            dataset.append((key, mfcc))
+
+    return dataset[:100]
+
+# Second dimension of the feature is dim2
+feature_dim_2 = 11
+print("The data before saving")
+# Save data to array file first
+save_data_to_array(max_len=feature_dim_2)
+print("The data is saved in NPY")
+# # Loading train set and test set
+X_train, X_test, y_train, y_test = get_train_test()
+
+# # Feature dimension
+feature_dim_1 = 20
+channel = 1
+epochs = 10
+batch_size = 100
+verbose = 1
+num_classes = 3
+
+# Reshaping to perform 2D convolution
+X_train = X_train.reshape(X_train.shape[0], feature_dim_1, feature_dim_2, channel)
+X_test = X_test.reshape(X_test.shape[0], feature_dim_1, feature_dim_2, channel)
+
+y_train_hot = to_categorical(y_train)
+y_test_hot = to_categorical(y_test)
+
+def get_model():
+    model = Sequential()
+    model.add(Conv2D(32, kernel_size=(2, 2), activation='relu', input_shape=(feature_dim_1, feature_dim_2, channel)))
+    model.add(Conv2D(48, kernel_size=(2, 2), activation='relu'))
+    model.add(Conv2D(120, kernel_size=(2, 2), activation='relu'))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+    model.add(Dropout(0.25))
+    model.add(Flatten())
+    model.add(Dense(128, activation='relu'))
+    model.add(Dropout(0.25))
+    model.add(Dense(64, activation='relu'))
+    model.add(Dropout(0.4))
+    model.add(Dense(num_classes, activation='softmax'))
+    model.compile(loss=keras.losses.categorical_crossentropy,
+                  optimizer=keras.optimizers.Adadelta(),
+                  metrics=['accuracy'])
+    return model
+	
+def predict(filepath, model):
+    sample = wav2mfcc(filepath)
+    sample_reshaped = sample.reshape(1, feature_dim_1, feature_dim_2, channel)
+    return get_labels()[0][np.argmax(model.predict(sample_reshaped))]
+	
+model = get_model()
+model.fit(X_train, y_train_hot, batch_size=batch_size, epochs=epochs, verbose=verbose, validation_data=(X_test, y_test_hot))
+# Predicts one sample
+
+
+print(predict('C:/Users/apt/Desktop/Data/TF/cat/0f250098_nohash_0.wav', model=model))
+
